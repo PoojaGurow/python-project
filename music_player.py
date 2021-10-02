70398d12fcbb940ceb4ea0ea5b554ec38a6550c2
Mukesh Singh <73029835+Mukesh108Singh@users.noreply.github.com>
2021-10-02 13:11:01 +0530

    Update music_player.py
    fixed some flaw in this code

--- music_player.py
+++ music_player.py
@@ -31,7 +31,7 @@ def unpause():
 Button1 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="PLAY", command=play, bg="blue", fg="white")
 Button2 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="STOP", command=stop, bg="red", fg="white")
 Button3 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="PAUSE", command=pause, bg="purple", fg="white")
-
+Button4 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="UNPAUSE", command=unpause, bg="orange", fg="white")
 
 var = tkr.StringVar() 
 song_title = tkr.Label(music_player, font="Helvetica 12 bold", textvariable=var)
@@ -40,7 +40,7 @@ song_title.pack()
 Button1.pack(fill="x")
 Button2.pack(fill="x")
 Button3.pack(fill="x")
-
+Button4.pack(fill="x")
 
 play_list.pack(fill="both", expand="yes")
 music_player.mainloop()
